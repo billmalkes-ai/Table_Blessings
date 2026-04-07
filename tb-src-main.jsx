c54c697b7b0c638d8811d375f5d57ceee82904bb
billmalkes-ai <bill.malkes@gmail.com>
2026-04-06 20:13:44 -0400

    Add files via upload

---
+++ tb-src-main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TableBlessings from '../TableBlessings.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TableBlessings />
+  </React.StrictMode>
+)
